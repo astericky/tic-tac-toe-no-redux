5db66d04b6ce6bf643ed3abe80e99f002f40a944
Chris Sanders <astericky@gmail.com>
2017-11-14 15:16:10 -0500

    update GameBoardButton

--- src/GameBoardButton.js
+++ src/GameBoardButton.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Button from 'antd/lib/button';
 
-const GameBoardButton = ({ type, label, xPos, yPos, hasWinner, handleButtonClicked }) => {
+const GameBoardButton = ({ type, label, xPos, yPos, isWinner, handleButtonClicked }) => {
     const handleClick = () => (handleButtonClicked(xPos, yPos)); 
     return (
         <Button
             type={ type || 'primary' }
             className="game-board-button"
-            disabled={hasWinner}
+            disabled={ isWinner }
             onClick={ handleClick }
         >
             { label || '\0' }
